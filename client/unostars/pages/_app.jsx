6069ea8a04804b99f93f3e2311c5e0820089650c
alejandra-sanchez <alejandra.sanchez@unosquare.com>
2022-01-19 15:44:47 -0600

    Theming added

--- client/unostars/pages/_app.jsx
+++ client/unostars/pages/_app.jsx
@@ -1,5 +1,25 @@
 import "../styles/globals.css";
 
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    type: "light",
+    primary: {
+      main: "#304FF3",
+    },
+    secondary: {
+      main: "#191919",
+    },
+    info: {
+      main: "#0098CE",
+    },
+  },
+  typography: {
+    fontFamily: "Lato",
+  },
+});
+
 function MyApp({ Component, pageProps }) {
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />;
