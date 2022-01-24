8eae2c0b8b7ecfcef1565325bb3ec7281c334fac
scabal-00 <samuel.cabal@unosquare.com>
2022-01-24 05:32:15 -0600

    implements the cloud - mongodb atlas

--- server/src/config.js
+++ server/src/config.js
@@ -7,8 +7,12 @@ const DB_NAME = process.env.DB_NAME || "unostar_development";
 const PORT = process.env.PORT || 3006;
 const JWT_SECRET = process.env.JWT_SECRET || "?,PXLYu1$wjH!TA";
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "1d";
+const MONGODB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://scabal-00:BufferedReader@cluster0.mxocf.mongodb.net/unostar_development?retryWrites=true&w=majority";
 
 module.exports = {
+  MONGODB_URI,
   IP_SERVER,
   PORT_DB,
   DB_NAME,
